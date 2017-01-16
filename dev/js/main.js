b9a4b3862842ffefe95cfadab873f20baa3575a3
Quentin <quentin.lemarchand@hetic.net>
2017-01-16 13:41:33 +0100

    final

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,55 +1,66 @@
+// mechanics var
 var dices           = [],
-selected_dices  = [],
-available_dices = 5,
-dices_number    = ['&#9856;', '&#9857;', '&#9858;', '&#9859;', '&#9860;', '&#9861'];
+	selected_dices  = [],
+	available_dices = 5,
+	dices_number    = ['&#9856;', '&#9857;', '&#9858;', '&#9859;', '&#9860;', '&#9861'];
 
+// view var
 var roll_btn   = document.querySelector('.roll'),
-html_dices = document.querySelectorAll('.dice');
+	html_dices = document.querySelectorAll('.dice');
 
-
+// roll event listener
 roll_btn.addEventListener('click', function(){
-	console.log('test');
+
 	random_dices();
 
 });
 
+// random dice
 function random_dices(){
 	
+	// get an empty dices storage
 	dices = [];
 
+	// generate dices
 	for(var i = 0; i < available_dices; i++){
 
-		var dice = {},
-		random_result = Math.floor(Math.random()*6);
+		var dice          = {},	// storage for new dice
+			random_result = Math.floor(Math.random()*6); // random dice result
 
+		// clear css classes
 		html_dices[i].className = "dice";
 
-		dice.result   = dices_number[random_result];
-		dice.selected = false;
+		dice.result   = dices_number[random_result]; // set result
+		dice.selected = false; // set false
 
+		// set index to html dices
 		html_dices[i].dataset.index = i;
 
+		// give random positionning
 		html_dices[i].classList.add("x"+Math.floor(Math.random()*5));
 		html_dices[i].classList.add("y"+Math.floor(Math.random()*5));
 
-		if(i > 0){
-			for(var j = 0; j < 5; j++){
+		// check if dices have the same position
+		for(var j = 0; j < 5; j++){
 
-				if(html_dices[i].classList[1] == html_dices[j].classList[1] && html_dices[i].classList[2] == html_dices[j].classList[2]){
+			if(i != j){
 
+				while(html_dices[i].classList[1] == html_dices[j].classList[1] && html_dices[i].classList[2] == html_dices[j].classList[2]){
+
+					// new positionning
 					html_dices[i].className = "dice";
 					html_dices[i].classList.add("x"+Math.floor(Math.random()*5));
 					html_dices[i].classList.add("y"+Math.floor(Math.random()*5));
-					console.log('done');
+
 				}
 
 			}
+
 		}
-		
+		// push the created dice in dices array
 		dices.push(dice);
-
 	}
-
+	// start html displaying function
 	display_html();
 
 }
@@ -58,20 +69,14 @@ function display_html(){
 
 	for(var i = 0; i < 5; i++){
 
-		if(dices[i] != selected_dices[i]){
-
-			html_dices[i].innerHTML = dices[i].result;
-
-		}/*else if(dices[i].selected){
-
-			html_dices[i].innerHTML = selected_dices[i].result;
-
-		}*/
+		// set the random value
+		html_dices[i].innerHTML = dices[i].result;
 
 	}
 
 }
 
+// listen each dice click event
 for(var i = 0; i < available_dices; i++){		
 
 	html_dices[i].addEventListener('click', function(e){
@@ -84,15 +89,14 @@ for(var i = 0; i < available_dices; i++){
 
 			dices[index].selected = false;
 
-		} else if(!dices[index].selected){
+		}else if(!dices[index].selected){
 
 			this.style.color = '#ff9900';
 
 			dices[index].selected = true;
 
-			selected_dices.push(dices[index]);
-
-			available_dices -= 1;
+			dices.splice(index, 1);	
+			console.log(dices);
 
 		}
 
